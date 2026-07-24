bab8e64eaaf8b34bf71d8f201818f238eff1683e
B Venkata Sailaja <bathinavenkatasailaja@gmail.com>
2026-07-24 08:37:40 +0530

    Modified file

--- Day01/Variables.py
+++ Day01/Variables.py
@@ -42,11 +42,3 @@ num2 = 20
 num1, num2 = num2, num1
 print("num1 =", num1)
 print("num2 =", num2)
-
-# ============================
-# 8. Check Data Types
-# ============================
-print(type(age))
-print(type(name))
-print(type(height))
-print(type(is_student))
